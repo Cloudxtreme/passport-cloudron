be738fbbd1c1f2160cb216b99edc11f75097bfac
Johannes Zellner <johannes@nebulon.de>
2015-04-07 18:13:01 +0200

    Add initial oauth client implementation

---
+++ index.js
@@ -0,0 +1,29 @@
+var util = require('util'),
+    superagent = require('superagent'),
+    OAuth2Strategy = require('passport-oauth').OAuth2Strategy,
+    InternalOAuthError = require('passport-oauth').InternalOAuthError;
+
+function Strategy(options, verify) {
+    options = options || {};
+    options.authorizationURL = options.authorizationURL || process.env.ADMIN_ORIGIN + '/api/v1/oauth/dialog/authorize';
+    options.tokenURL = options.tokenURL || process.env.ADMIN_ORIGIN + '/api/v1/oauth/token';
+    options.scopeSeparator = options.scopeSeparator || ',';
+    options.clientID = options.clientID || process.env.OAUTH_CLIENT_ID;
+    options.clientSecret = options.clientSecret || process.env.OAUTH_CLIENT_SECRET;
+
+    OAuth2Strategy.call(this, options, verify);
+    this.name = 'cloudron';
+}
+
+util.inherits(Strategy, OAuth2Strategy);
+
+Strategy.prototype.userProfile = function(accessToken, done) {
+    superagent.get(apiOrigin + '/api/v1/profile').query({ access_token: req.session.token }).end(function (error, result) {
+        if (error) return done(new InternalOAuthError('failed to fetch user profile', error));
+        if (result.status !== 200) done(new InternalOAuthError('failed to fetch user profile', result.status));
+
+        done(null, result.body);
+    });
+};
+
+module.exports = Strategy;
