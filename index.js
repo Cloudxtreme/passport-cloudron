c6971610fffb135107635af4145cd2a9a6757376
Johannes Zellner <johannes@nebulon.de>
2015-04-07 18:50:59 +0200

    Fixup bugs

--- index.js
+++ index.js
@@ -8,23 +8,26 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 function Strategy(options, verify) {
     options = options || {};
-    options.authorizationURL = options.authorizationURL || process.env.ADMIN_ORIGIN + '/api/v1/oauth/dialog/authorize';
-    options.tokenURL = options.tokenURL || process.env.ADMIN_ORIGIN + '/api/v1/oauth/token';
-    options.scopeSeparator = options.scopeSeparator || ',';
+    options.adminOrigin = options.adminOrigin || process.env.ADMIN_ORIGIN;
+
+    options.authorizationURL = options.authorizationURL || options.adminOrigin + '/api/v1/oauth/dialog/authorize';
+    options.tokenURL = options.tokenURL || options.adminOrigin + '/api/v1/oauth/token';
+    options.scopeSeparator = ',';
+
     options.clientID = options.clientID || process.env.OAUTH_CLIENT_ID;
     options.clientSecret = options.clientSecret || process.env.OAUTH_CLIENT_SECRET;
 
     OAuth2Strategy.call(this, options, verify);
     this.name = 'cloudron';
+    this._profileUrl = options.adminOrigin + '/api/v1/profile';
 }
 
 util.inherits(Strategy, OAuth2Strategy);
 
-Strategy.prototype.userProfile = function(accessToken, done) {
-    superagent.get(apiOrigin + '/api/v1/profile').query({ access_token: req.session.token }).end(function (error, result) {
+Strategy.prototype.userProfile = function (accessToken, done) {
+    superagent.get(this._profileUrl).query({ access_token: accessToken }).end(function (error, result) {
         if (error) return done(new InternalOAuthError('failed to fetch user profile', error));
-        if (result.status !== 200) done(new InternalOAuthError('failed to fetch user profile', result.status));
-
+        if (result.statusCode !== 200) return done(new InternalOAuthError('failed to fetch user profile', result.statusCode));
         done(null, result.body);
     });
 };
